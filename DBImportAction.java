699d33084407838713a34e92946a74a73c75e898
Tushinsky <67974097+Tushinsky@users.noreply.github.com>
2023-07-24 16:06:39 +0300

    Add files via upload

--- DBImportAction.java
+++ DBImportAction.java
@@ -5,24 +5,80 @@
  */
 package admintools;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+
 /**
  *
  * @author Sergii.Tushinskyi
  */
 public class DBImportAction extends DBOperation {
 
-    public DBImportAction() {
-        super();
+    private final int importStep = 4;// количество шагов для операции импорта
+    private int step = 0;// номер шага для выбранной операции с базой данных
+    
+    public DBImportAction(JList lstTableName, JList lstTargetList, 
+            JTextArea txtStep, JLabel lblTableName, JLabel lblTargetLabel, 
+            JDBCConnection connection) {
+        super(connection, lstTableName, lstTargetList, txtStep, lblTableName, lblTargetLabel);
     }
 
     @Override
     public void moveNext() {
-        super.moveNext(); //To change body of generated methods, choose Tools | Templates.
+        //To change body of generated methods, choose Tools | Templates.
+        // увеличиваем шаг
+        step++;
+        if(step <= importStep){
+            importData();
+        } else{
+            step--;
+            }
     }
 
     @Override
     public void movePreviouse() {
-        super.movePreviouse(); //To change body of generated methods, choose Tools | Templates.
+        //To change body of generated methods, choose Tools | Templates.
+        // уменьшаем шаг
+        step--;
+        if(step >= 0){
+
+            importData();
+        } else{
+            step = 0;
+        }
     }
     
+    private void importData() {
+        switch(step) {
+            case 0:
+                // получение списка таблиц базы данных, выбор таблицы для импорта
+                // получаем список таблиц
+                Object[] tableName = super.getTable_Name();
+                // модель списка
+                DefaultListModel model = super.model(tableName);
+                super.setListModel(super.getLstTableName(), model);
+                super.getTxtStep().setText("Шаг 1: выберите таблицу для импорта данных");
+                super.getLblTableName().setText("список таблиц");
+                super.getLblTargetLabel().setText("выбранная таблица");
+                break;
+            case 1:
+                // получение перечня полей выбранной таблицы, выбор полей для импорта
+                Object[] columnName = super.getColumn_Name();// получаем список полей
+                break;
+            case 2:
+                // сопоставление полей для импорта
+                break;
+            case 3:
+                // извещение о том, что можно делать импорт
+                break;
+            case 4:
+                /**
+                 * выполнение импорта данных, подсчёт количества импортируемых строк,
+                 * подсчёт количества ошибок импорта, извещение о количестве импорта и ошибок
+                **/
+                break;
+        }
+    }
 }
--- DBOperation.java
+++ DBOperation.java
@@ -5,19 +5,106 @@
  */
 package admintools;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+
 /**
  *
  * @author Sergii.Tushinskyi
  */
 public class DBOperation implements MoveOnStepImpl {
 
+    /**
+     * @return the lstTableName
+     */
+    public JList getLstTableName() {
+        return lstTableName;
+    }
+
+    /**
+     * @param lstTableName the lstTableName to set
+     */
+    public void setLstTableName(JList lstTableName) {
+        this.lstTableName = lstTableName;
+    }
+
+    /**
+     * @return the lstTargetList
+     */
+    public JList getLstTargetList() {
+        return lstTargetList;
+    }
+
+    /**
+     * @param lstTargetList the lstTargetList to set
+     */
+    public void setLstTargetList(JList lstTargetList) {
+        this.lstTargetList = lstTargetList;
+    }
+
+    /**
+     * @return the txtStep
+     */
+    public JTextArea getTxtStep() {
+        return txtStep;
+    }
+
+    /**
+     * @param txtStep the txtStep to set
+     */
+    public void setTxtStep(JTextArea txtStep) {
+        this.txtStep = txtStep;
+    }
+
+    /**
+     * @return the lblTableName
+     */
+    public JLabel getLblTableName() {
+        return lblTableName;
+    }
+
+    /**
+     * @param lblTableName the lblTableName to set
+     */
+    public void setLblTableName(JLabel lblTableName) {
+        this.lblTableName = lblTableName;
+    }
+
+    /**
+     * @return the lblTargetLabel
+     */
+    public JLabel getLblTargetLabel() {
+        return lblTargetLabel;
+    }
+
+    /**
+     * @param lblTargetLabel the lblTargetLabel to set
+     */
+    public void setLblTargetLabel(JLabel lblTargetLabel) {
+        this.lblTargetLabel = lblTargetLabel;
+    }
+
     private JDBCConnection connection;
     private Object[] table_Name;// массив с именами таблиц базы данных
     private Object[] column_Name;// массив с именами полей выбранной таблицы
+    private String table;// имя выбранной таблицы
+    private JList lstTableName;// список с именами таблиц базы данных
+    private JList lstTargetList;// целевой список
+    private JTextArea txtStep;// поле ввода с описание шагов операции
+    private JLabel lblTableName;// метка для списка с именами таблиц
+    private JLabel lblTargetLabel;// метка для целевого списка
 
-
-    public DBOperation(JDBCConnection connection) {
+    public DBOperation(JDBCConnection connection, JList lstTableName, 
+            JList lstTargetList, JTextArea txtStep, JLabel lblTableName, 
+            JLabel lblTargetLabel) {
         this.connection = connection;
+        this.lstTableName = lstTableName;
+        this.lstTargetList = lstTargetList;
+        this.txtStep = txtStep;
+        this.lblTableName = lblTableName;
+        this.lblTargetLabel = lblTargetLabel;
     }
 
     public DBOperation() {
@@ -54,12 +141,55 @@ public class DBOperation implements MoveOnStepImpl {
         return getDBTableColumnName();
     }
     
+    /**
+     * 
+     * @param nameArray
+     * @return 
+     */
+    public DefaultListModel model (Object[] nameArray) {
+        DefaultListModel retModel = new DefaultListModel();
+        retModel.clear();
+        // если передан массив и он содержит данные
+        if(nameArray != null && nameArray.length > 0){
+            for (Object nameArray1 : nameArray) {
+                retModel.addElement(nameArray1.toString());
+            }
+        }
+        return retModel;
+    }
+    
+    /**
+    * устанавливает модель для списка
+     * @param list
+     * @param model
+    */
+    public void setListModel(JList list, DefaultListModel model){
+            list.setModel(model);
+    }
+
+    
     private Object[] getDBTableName() {
-        return null;
+        table_Name = connection.getListTable();
+        return table_Name;
         
     }
     
     private Object[] getDBTableColumnName() {
-        return null;
+        column_Name = connection.getListColumnTable(getTable());
+        return column_Name;
+    }
+
+    /**
+     * @return the table
+     */
+    public String getTable() {
+        return table;
+    }
+
+    /**
+     * @param table the table to set
+     */
+    public void setTable(String table) {
+        this.table = table;
     }
 }
