ac6161cf430558832a0ab27c6c8cd71ab09d963f
Tushinsky <67974097+Tushinsky@users.noreply.github.com>
2023-07-30 20:53:56 +0300

    Add files via upload (#11)
    добавлен класс лдя обновления данных

---
+++ DBUpdateAction.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admintools;
+
+/**
+ *
+ * @author Sergey
+ */
+public class DBUpdateAction extends DBOperation{
+
+    public DBUpdateAction() {
+    }
+
+    @Override
+    public void moveNext() {
+        super.moveNext(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void movePreviouse() {
+        super.movePreviouse(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
