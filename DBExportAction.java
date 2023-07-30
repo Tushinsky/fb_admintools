426606686d851ef17521b9b601eb9a630ed48487
Tushinsky <67974097+Tushinsky@users.noreply.github.com>
2023-07-30 20:51:17 +0300

    Add files via upload (#10)

---
+++ DBExportAction.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admintools;
+
+/**
+ *
+ * @author Sergey
+ */
+public class DBExportAction extends DBOperation {
+
+    public DBExportAction() {
+    }
+
+    @Override
+    public void moveNext() {
+        super.moveNext(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void movePreviouse() {
+        super.movePreviouse(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
